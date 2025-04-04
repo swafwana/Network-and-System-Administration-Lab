39cc9e596fadab8181fc51fe4b3e9ce2f0f22466
swafwana <swafwanasheri17@gmail.com>
2025-04-04 11:18:34 +0530

    Add files via upload

--- Week8/extract.sh
+++ Week8/extract.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-file="filename.txt"
+echo "Enter a file name"
+read file
 echo "Enter the starting line number of set of lines to extract: "
 read sno
-echo "Enter the starting line number of set of lines to extract: "
+echo "Enter the ending line number of set of lines to extract: "
 read eno
 
-sed -n "${sno},${eno}p" "$file8"
+sed -n "${sno},${eno}p" "$file"
 
 
